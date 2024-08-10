f6b10d0af662064674945a3b3ca7c5400067b2a2
OmkarShinde09 <omkarshine07091969@gmail.com>
2024-08-10 10:19:04 +0530

    made the page responsive

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function page4Animation(){
+
+function page4Animation() {
     var elemC = document.querySelector("#elem-container")
     var fixed = document.querySelector("#fixed-image")
     elemC.addEventListener("mouseenter", function () {
@@ -12,7 +13,7 @@ function page4Animation(){
     elemC.addEventListener("mouseleave", function () {
         fixed.style.display = "none"
     })
-    
+
     var elems = document.querySelectorAll(".elem")
     elems.forEach(function (e) {
         e.addEventListener("mouseenter", function () {
@@ -22,17 +23,32 @@ function page4Animation(){
     })
 }
 
-function swiperAnimation(){
+function swiperAnimation() {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
         centeredSlides: true,
-        spaceBetween: 50,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
+        spaceBetween: 100,
     });
 }
+function menuAnimation() {
 
-page4Animation();
-swiperAnimation();
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+swiperAnimation()
+page4Animation()
+menuAnimation()
